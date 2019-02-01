cea573e82e88d1f205263794718854f6da091259
David Wheatley <blokmaster.help@gmail.com>
2019-02-01 21:54:56 +0000

    Adds notification for build start

--- send.sh
+++ send.sh
@@ -10,19 +10,24 @@ case $1 in
   "success" )
     EMBED_COLOR=3066993
     STATUS_MESSAGE="Passed"
-    AVATAR="https://travis-ci.org/images/logos/TravisCI-Mascot-blue.png"
+    AVATAR="https://travis-ci.com/images/logos/TravisCI-Mascot-blue.png"
     ;;
 
   "failure" )
     EMBED_COLOR=15158332
     STATUS_MESSAGE="Failed"
-    AVATAR="https://travis-ci.org/images/logos/TravisCI-Mascot-red.png"
+    AVATAR="https://travis-ci.com/images/logos/TravisCI-Mascot-red.png"
     ;;
+    
+  "build-start )
+    EMBED_COLOR=14396169
+    STATUS_MESSAGE="Build starting..."
+    AVATAR="https://travis-ci.com/images/logos/TravisCI-Mascot-grey.png"
 
   * )
     EMBED_COLOR=0
     STATUS_MESSAGE="Status Unknown"
-    AVATAR="https://travis-ci.org/images/logos/TravisCI-Mascot-1.png"
+    AVATAR="https://travis-ci.com/images/logos/TravisCI-Mascot-1.png"
     ;;
 esac
 
